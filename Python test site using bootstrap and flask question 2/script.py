bf6211156ce82d7ebc298364f0171a768d0b599d
Adam Smith <adam.k.smith13@gmail.com>
2025-01-16 02:18:25 -0700

    Update script.py
    updated script.py for question 2

--- Python test site using bootstrap and flask question 2/script.py
+++ Python test site using bootstrap and flask question 2/script.py
@@ -56,4 +56,5 @@ def run_code():
     return jsonify({"results": results})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
