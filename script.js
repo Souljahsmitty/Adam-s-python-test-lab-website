dbf6088bd13619e137d3b28a2897275dfb348cc5
Adam Smith <adam.k.smith13@gmail.com>
2025-01-16 00:16:31 -0700

    Update script.js
    updated the fetch function in Java

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let correctCount = 0;
         resultContainer.innerHTML = ""; // Clear previous results
 
-        fetch("http://127.0.0.1:5000/run_code", {
+        fetch("https://adam-s-python-test-lab-website.onrender.com/run_code", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
